5e0832e0d2dd653bffa903a6efb1c49a757a2e67
Sharafuddin <esharafu@nrel.gov>
2023-06-15 10:02:27 -0600

    added values up to cost breakdown

--- green_concrete.py
+++ green_concrete.py
@@ -5,12 +5,15 @@ Created on Wed June 14 2:28 2023
 @author: evan-sharafuddin
 """
 
+import ProFAST
+
 # Abbreviations
 # LHV - lower heat value 
 # ng - natural gas
 # clk - clinker
 # tdc - total direct costs (see paper)
 # epc - engineering process construction ?
+# om - O&M
 
 # TODO
 # does 't' mean short ton, tonne, etc?
@@ -109,8 +112,79 @@ class ConcretePlant:
             owner_costs = 0.07 # % TDC
             project_contingency = 0.15 # % TDC
             owner_cost_conting = total_direct_cost * (owner_costs + project_contingency) # M€
-            total_plant_cost = epc + owner_cost_conting + total_plant_cost # M€ (kiln and capture system)
+            total_plant_cost = epc + owner_cost_conting + total_plant_cost # M€ (kiln and capture system, if CSS enabled)
         
+        # ---------------------------------- OPEX ----------------------------------
+        ## carbon tax
+        ### in spreadsheet, skipping for now ###
+
+        total_consumables = raw_meal * raw_meal_clk + \
+                            fuel * fuel_clk + \
+                            electricity * electricity_clk + \
+                            steam * steam_clk + \
+                            cooling_water * cooling_water_clk + \
+                            ammonia * ammonia_clk # €/ton clk
+        
+        other_var_om = 0.8 # €/ton cement
+        other_var_om = 0.8 * cement_clinker_ratio # €/ton clk
+
+        insurance_local_tax_rate = 0.02 # % TPC
+        insurance_local_tax = total_plant_cost * insurance_local_tax_rate # M€/year
+
+        maintenance_cost_rate = 0.02 # % TPC
+        maintenance = maintenance_cost_rate * total_plant_cost # M€/year
+        ## labor
+        if ccs:
+            num_persons = 120
+        else:
+            num_persons = 100
+
+        labor_per_person = 60 # k€/year/person
+        operating_labor = labor_per_person * num_persons * 1e3 / 1e6 # M€/year
+        
+        
+        maintenance_labor_rate = 0.4 # % of maintenance cost
+        maintenance_labor = maintenance_labor_rate * maintenance # M€/year
+
+        admin_support__rate = 0.3 # % oper. and maintenance labor cost
+        admin_support = admin_support__rate * (operating_labor * maintenance_labor) # M€/year
+
+        labor_cost = operating_labor + admin_support # M€/year
+
+        # ---------------------------------- ProFAST ----------------------------------
+        pf = ProFAST.ProFAST('blank')
+        gen_inflation = 0.00
+        pf.set_params('commodity',{"name":'Clinker',"unit":"ton","initial price":1000,"escalation":gen_inflation})
+        pf.set_params('capacity',plant_capacity_mtpy/365) #units/day
+        pf.set_params('maintenance',{"value":0,"escalation":gen_inflation})
+        pf.set_params('analysis start year',2022)
+        pf.set_params('operating life',plant_life)
+        pf.set_params('installation months',20)
+        pf.set_params('installation cost',{"value":installation_cost,"depr type":"Straight line","depr period":4,"depreciable":False})
+        pf.set_params('non depr assets',land_cost)
+        pf.set_params('end of proj sale non depr assets',land_cost*(1+gen_inflation)**plant_life)
+        pf.set_params('demand rampup',5.3)
+        pf.set_params('long term utilization',plant_capacity_factor)
+        pf.set_params('credit card fees',0)
+        pf.set_params('sales tax',0) 
+        pf.set_params('license and permit',{'value':00,'escalation':gen_inflation})
+        pf.set_params('rent',{'value':0,'escalation':gen_inflation})
+        pf.set_params('property tax and insurance percent',0)
+        pf.set_params('admin expense percent',0)
+        pf.set_params('total income tax rate',0.27)
+        pf.set_params('capital gains tax rate',0.15)
+        pf.set_params('sell undepreciated cap',True)
+        pf.set_params('tax losses monetized',True)
+        pf.set_params('operating incentives taxable',True)
+        pf.set_params('general inflation rate',gen_inflation)
+        pf.set_params('leverage after tax nominal discount rate',0.0824)
+        pf.set_params('debt equity ratio of initial financing',1.38)
+        pf.set_params('debt type','Revolving debt')
+        pf.set_params('debt interest rate',0.0489)
+        pf.set_params('cash onhand percent',1)
+
+        
+
 
 
     def portland_tea(): # OPC-TEA
@@ -119,5 +193,19 @@ class ConcretePlant:
         
     def run_profast_for_concrete():
         pass
+
+    # ---------------------------------- Misc. Functions ----------------------------------
+    def call_profast
+    
+    def euro_to_usd(cost_EUR):
+        ''' 
+        Converts monetary values from EUR to USD
+
+        NOTE: conversion factor is the average from 2014, which was the cost basis
+        year given in the spreadsheet
+        
+        '''
+        conversion_factor = 1.3283 # USD/EUR
+        return conversion_factor * cost_EUR
 
 
